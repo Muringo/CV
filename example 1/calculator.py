d3a60b22aa2ecaae2a1fbd173a3910f9bf34d6ce
Muringo <47770423+Muringo@users.noreply.github.com>
2019-04-23 03:54:43 -0700

    Add files via upload
    EXAMPLE 1 WORK

---
+++ example 1/calculator.py
@@ -0,0 +1,26 @@
+def add(a,b):
+    answer = a+b
+    return answer
+def substract(a,b):
+	answer=a-b
+	return answer
+def multiply(a,b):
+	answer=a*b
+	return answer
+def divide(a,b):
+	answer=a/b
+	return answer
+def module(a,b):
+	answer=a%b
+	return answer
+def exponent(a,b):
+	answer=a**b
+	return answer
+def percentage(a,b):
+	answer=a%b 
+	return answer
+def wholenumber(a,b):
+	answer=a//b
+	return answer
+
+
